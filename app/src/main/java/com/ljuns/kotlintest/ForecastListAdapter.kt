a4902c1d61da4873fdebabf01743286b198846bc
ljuns <ljuns.dev@gmail.com>
2018-11-28 13:32:27 +0800

    使用 RecyclerView 展示静态数据

---
+++ app/src/main/java/com/ljuns/kotlintest/ForecastListAdapter.kt
@@ -0,0 +1,24 @@
+package com.ljuns.kotlintest
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @author ljuns
+ * Created at 2018/11/28.
+ */
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
--- app/src/main/java/com/ljuns/kotlintest/MainActivity.kt
+++ app/src/main/java/com/ljuns/kotlintest/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.ljuns.kotlintest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // 创建数据集合
+    private val items = listOf(
+        "Mon 6/23 - Sunny - 31/17",
+        "Tue 6/24 - Foggy - 21/8",
+        "Wed 6/25 - Cloudy - 22/17",
+        "Thurs 6/26 - Rainy - 18/11",
+        "Fri 6/27 - Foggy - 21/10",
+        "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+        "Sun 6/29 - Sunny - 20/7")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 设置 LayoutManager
+        forecastList.layoutManager = LinearLayoutManager(this)
+        // 设置 Adapter
+        forecastList.adapter = ForecastListAdapter(items)
+
     }
 }
